77a123c5f466e0d15304d66c62df0e34db160494
KovalevRus <120650333+KovalevRus@users.noreply.github.com>
2025-05-18 20:54:04 +0300

    Update firebase_config_loader.py

--- firebase_config_loader.py
+++ firebase_config_loader.py
@@ -1,21 +1,26 @@
 import os
 import json
-import tempfile
 import firebase_admin
-from firebase_admin import credentials
+from firebase_admin import credentials, firestore
+
+firebase_app = None
+db = None
 
 def initialize_firebase():
-    if firebase_admin._apps:
-        return  # Firebase уже инициализирован
+    global firebase_app, db
 
-    firebase_json = os.getenv("FIREBASE_JSON")
-    if not firebase_json:
-        raise RuntimeError("FIREBASE_JSON не установлена")
+    if firebase_app is not None:
+        return db
 
-    # Сохраняем содержимое переменной во временный файл
-    with tempfile.NamedTemporaryFile(mode="w+", delete=False) as temp_file:
-        temp_file.write(firebase_json)
-        temp_path = temp_file.name
+    firebase_config_json = os.getenv("GOOGLE_CREDENTIALS")
+    if not firebase_config_json:
+        raise ValueError("Переменная окружения GOOGLE_CREDENTIALS не установлена.")
 
-    cred = credentials.Certificate(temp_path)
-    firebase_admin.initialize_app(cred)
+    try:
+        firebase_config = json.loads(firebase_config_json)
+        cred = credentials.Certificate(firebase_config)
+        firebase_app = firebase_admin.initialize_app(cred)
+        db = firestore.client()
+        return db
+    except Exception as e:
+        raise RuntimeError(f"Ошибка при инициализации Firebase: {e}")
